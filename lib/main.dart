5079f8bfaf04df2208a56bc7f36029959ce11e64
ZeekHoft <dltposiedon@gmail.com>
2024-06-27 00:16:49 +0800

    made changes on main.dart, took out comments and added const

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
-//import 'package:firebase_core/firebase_core.dart';
-//import 'package:flutter/foundation.dart';
 import 'package:cs_compas/controllers/auth.dart';
 import 'package:cs_compas/firebase_options.dart';
 import 'package:cs_compas/pages/signup.dart';
@@ -23,7 +19,7 @@ Future main() async {
         '/': (context) => CheckUser(),
         '/signup': (context) => Signup(),
         '/login': (context) => Login(),
-        '/home': (context) => Home(
+        '/home': (context) => const Home(
               email:
                   'hey this is one of the creators vince, if you managed to see this CONGRATULATIONS!!',
               idnumber: 'thats it thats the easter egg',
